68115156d6c2f02b418c68264c47ad610bbdd9e1
ALEJANDRA NAVARRO LAZCANO <alenavalazca@gmail.com>
2019-10-18 14:52:34 -0500

    Creación de clase Shop e incorporación de int/string/double de nuestras vistas.

---
+++ TiendaMachingon.Web/Data/Entities/Shop.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TiendaMachingon.Web.Data.Entities
+{
+    public class Shop
+    {
+        public int Customer { get; set; }
+
+        public double Sale { get; set; }
+
+        public int SaleDetail { get; set; }
+
+        public int Product { get; set; }
+
+        public int Provider { get; set; }
+
+        public double Purchase { get; set; }
+
+        public int PurchaseDetail { get; set; }
+    }
+}
