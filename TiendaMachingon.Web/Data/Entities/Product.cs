575334a38732e4b46103f0dcda95ab5a8abebf5e
ALEJANDRA NAVARRO LAZCANO <alejandra.navarrola@my.uvm.edu.mx>
2019-10-23 17:13:52 -0500

    Product.cs "using System.ComponentModel.DataAnnotations;"

--- TiendaMachingon.Web/Data/Entities/Product.cs
+++ TiendaMachingon.Web/Data/Entities/Product.cs
@@ -8,13 +8,20 @@
 
         [Required(ErrorMessage ="El campo {0} es requerido")]
         [MaxLength(50, ErrorMessage ="El campo {0} debe tener máximo {1} caracteres")]
-        [Display(Name="Nombre del producto")]
+        [Display(Name="Nombre del producto->")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [Display(Name = "Precio")]
         public decimal Price { get; set; }
 
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [Display(Name = "Cantidad")]
         public int Quantity { get; set; }
 
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caracteres")]
+        [Display(Name = "Descripción del producto->")]
         public string Description { get; set; }
     }
 }
