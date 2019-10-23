69ba7d485f180bff8180aee149486e479b5f8594
ALEJANDRA NAVARRO LAZCANO <alejandra.navarrola@my.uvm.edu.mx>
2019-10-23 17:08:03 -0500

    Se agregó en Product.cs: [Required(ErrorMessage ="El campo {0} es requerido")]         [MaxLength(50, ErrorMessage ="El campo {0} debe tener máximo {1} caracteres")]         [Display(Name="Nombre del producto")]

--- TiendaMachingon.Web/Data/Entities/Product.cs
+++ TiendaMachingon.Web/Data/Entities/Product.cs
@@ -1,13 +1,20 @@
 ﻿namespace TiendaMachingon.Web.Data.Entities
 {
+    using System.ComponentModel.DataAnnotations;
     public class Product: IEntity
     {
+        [key]
         public int Id { get; set; }
 
+        [Required(ErrorMessage ="El campo {0} es requerido")]
+        [MaxLength(50, ErrorMessage ="El campo {0} debe tener máximo {1} caracteres")]
+        [Display(Name="Nombre del producto")]
         public string Name { get; set; }
 
-        public decimal Precio { get; set; }
+        public decimal Price { get; set; }
 
-        public int MyProperty { get; set; }
+        public int Quantity { get; set; }
+
+        public string Description { get; set; }
     }
 }
