977f9b8a114977c0d9f69c061e1347d16e85055e
ALEJANDRA NAVARRO LAZCANO <alenavalazca@gmail.com>
2019-10-27 15:12:26 -0600

    Creación de clase Sale con sus respectivos get; set;

---
+++ TiendaMachingon.Web/Data/Entities/Sale.cs
@@ -0,0 +1,23 @@
+﻿namespace TiendaMachingon.Web.Data.Entities
+{
+    public class Sale: IEntity
+    {
+        [key]
+
+        public int Id { get; set; }
+
+        public double Date { get; set; }
+
+        public int Subtotal { get; set; }
+
+        public int IVA { get; set; }
+
+        public int TotalSale { get; set; }
+
+        public int Quantity { get; set; }
+
+        public string PaymentType { get; set; }
+        
+
+    }
+}
